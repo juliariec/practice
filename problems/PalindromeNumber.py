934ee2847ea545c6cf2b148da75f5e791a1f1179
Julia Cooke <juliariec@gmail.com>
2022-01-16 17:04:00 -0500

    Improved solution, thanks @johannestreutlein

--- problems/PalindromeNumber.py
+++ problems/PalindromeNumber.py
@@ -1,19 +1,19 @@
 # Solution
 def isPalindrome(x: int) -> bool:
-    valid = True
     if (x >= 0):
         strX = str(x)
-        for i in range(0, len(strX)//2+1):
+        for i in range(0, len(strX) // 2):
             if (strX[i] != strX[-(i+1)]):
-                valid = False
+                return False
     else:
-        valid = False
-    return valid
+        return False
+    return True
 
 # Tests
 assert isPalindrome(323) == True
 assert isPalindrome(1) == True
 assert isPalindrome(0) == True
+assert isPalindrome(224535422) == True
 assert isPalindrome(-121) == False
 assert isPalindrome(998) == False
 assert isPalindrome(77557) == False
